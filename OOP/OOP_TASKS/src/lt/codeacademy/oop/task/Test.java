6f355b02d2916fc43ddba43d1856d11508ef5f86
Rombug <119162581+Rombug@users.noreply.github.com>
2023-01-15 16:24:35 +0200

    Repo test

--- OOP/OOP_TASKS/src/lt/codeacademy/oop/task/Test.java
+++
@@ -1,7 +0,0 @@
-package lt.codeacademy.oop.task;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("Repo skirtos atsiskaitymams pridejimas");
-    }
-}
---
+++ OOP/task/src/lt/codeacademy/oop/task/examresult/Testa.java
@@ -0,0 +1,7 @@
+package lt.codeacademy.oop.task.examresult;
+
+public class Testa {
+    public static void main(String[] args) {
+        System.out.println("Test repo");
+    }
+}
